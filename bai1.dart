4e65ad725398a4c1c451035ab313c326f56a4b64
LeMinhHieu <126046657+LemhHieu@users.noreply.github.com>
2023-03-15 13:48:00 +0700

    Update bai1.dart

--- bai1.dart
+++ bai1.dart
@@ -4,7 +4,7 @@ void main() {
   // open file
   File file = File('hello.txt');
   // write to file
-  String myName = 'Nguyễn Quý Hải';
+  String myName = 'Lê Minh Hiếu';
   file.writeAsStringSync('Hello ${myName}\n', mode: FileMode.append);
   print('Congratulations!! Name is added');
 }
